a8ce82fbb1a089bcebcd9731969492aa93407ccd
Haneen Mahdin <haneenmahdin@gmail.com>
2022-02-04 18:07:20 +0530

    Test install

--- tests/isInstalled.test.js
+++ tests/isInstalled.test.js
@@ -1,8 +1,18 @@
+const install = require("../lib/install");
 const isInstalled = require("../lib/utils/isInstalled");
 
 test('Check if the packages was installed', () => {
-  expect(isInstalled("@types/node")).toBe(true);
-  expect(isInstalled("jest")).toBe(true);
-  expect(isInstalled("play-sound")).toBe(true);
+  try {
+    install("@types/node", true);
+  } finally {
+    expect(isInstalled("@types/node")).toBe(true);
+  }
+
+  try {
+    install("express", false);
+  } finally {
+    expect(isInstalled("express")).toBe(true);
+  }
+  
   expect(isInstalled("\\asaa\a\saasaa\as\asa\a\\a\\sa")).toBe(false); // check if this failes
 });
