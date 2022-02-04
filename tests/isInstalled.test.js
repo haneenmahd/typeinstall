f23408940600da226367187f51b7e8766f97f31a
Haneen Mahdin <haneenmahdin@gmail.com>
2022-02-04 18:21:39 +0530

    expect packages to be installed after `install` script run

--- tests/isInstalled.test.js
+++ tests/isInstalled.test.js
@@ -1,18 +1,11 @@
-const install = require("../lib/install");
+/**
+ * Before running some scripts in here, install these packages using the `install` script from the command line.
+ */
 const isInstalled = require("../lib/utils/isInstalled");
 
-test('Check if the packages was installed', () => {
-  try {
-    install("@types/node", true);
-  } finally {
-    expect(isInstalled("@types/node")).toBe(true);
-  }
+test("Check if the packages was installed", () => {
+  expect(isInstalled("@types/node")).toBe(true);
+  expect(isInstalled("play-sound")).toBe(true);
 
-  try {
-    install("express", false);
-  } finally {
-    expect(isInstalled("express")).toBe(true);
-  }
-  
-  expect(isInstalled("\\asaa\a\saasaa\as\asa\a\\a\\sa")).toBe(false); // check if this failes
+  expect(isInstalled("\\asaaasaasaaasasaa\\a\\sa")).toBe(false); // check if this failes
 });
