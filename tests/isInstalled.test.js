15a102a23f4fd5d81a1ace8b087cd38b926bb7d6
Haneen Mahdin <haneenmahdin@gmail.com>
2022-02-05 08:14:22 +0530

    ci: try fixing tests

--- tests/isInstalled.test.js
+++ tests/isInstalled.test.js
@@ -4,8 +4,8 @@
 const isInstalled = require("../lib/utils/isInstalled");
 
 test("Check if the packages was installed", () => {
-  expect(isInstalled("@types/cors")).toBe(true);
-  expect(isInstalled("cors")).toBe(true);
+  expect(isInstalled("@types/express")).toBe(true);
+  expect(isInstalled("express")).toBe(true);
 
   expect(isInstalled("\\asaaasaasaaasasaa\\a\\sa")).toBe(false); // check if this failes
 });
